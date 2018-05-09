2979c518ddf959690b74534e25584a21d5cfc9ba
Frank Hu o <yuandahuo@gmail.com>
2018-05-09 20:30:01 +1200

    finished layout

---
+++ src/Component/Layout.js
@@ -0,0 +1,103 @@
+import React from 'react'
+ import {Layout, Menu, Breadcrumb,Icon} from 'antd';
+// import {
+//   // BrowserRouter as Router,
+//   Route,
+//   // Switch,
+//   Link
+// } from 'react-router-dom';
+// // 按需加载
+// import async from '../async';
+// import './Layout.css';
+// let ButtonDemo = async(() => import("./ButtonDemo"));
+// let MyTable = async(() => import("./Table"));
+// /*****************************/
+ const { Header, Content, Footer, Sider } = Layout;
+ const SubMenu = Menu.SubMenu;
+
+// import ButtonDemo from './ButtonDemo'
+// import InputDemo from './InputDemo'
+
+
+class MyLayout extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+          trigger={null}
+        >
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Home Page</span>
+              {/* <Link to='/'></Link> */}
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Table</span>
+              {/* <Link to='/Table'></Link> */}
+              {/* <Link to="/"></Link> */}
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={<span><Icon type="user" /><span>User</span></span>}
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={<span><Icon type="team" /><span>Team</span></span>}
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} >
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              {/* <Breadcrumb.Item>Bill</Breadcrumb.Item> */}
+            </Breadcrumb>
+            {/* <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+            </div> */}
+            {/* <Route path='/' exact component={ButtonDemo}></Route>
+            <Route path='/Table' exact component={MyTable}></Route> */}
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default MyLayout
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import MyLayout from './Component/Layout';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyLayout />, document.getElementById('root'));
 registerServiceWorker();
